f071cfc2c27e8cadd3b8bb561bfc083f61c2bbb0
Mikael Ly <mikael2aa@gmail.com>
2025-11-25 02:39:31 -0700

    changed made to index
    - added continue learning + categories sections to index.html
    
    - changed position of the user icon, adjusted some info to better fit other pages
    
    - added progress bar + color to continue learning
    
    - added begin random quiz button
    
    - added link to progress on index
    
    - created CategoryCard.tsx component

--- app/index.tsx
+++ app/index.tsx
@@ -6,13 +6,18 @@ Authors: Mikael Ly, Minh Tam Nguyen, Xiaomei He
 
 */
 
-import AntDesign from '@expo/vector-icons/AntDesign';
+import CategoryCard from '@/components/CategoryCard';
 import Entypo from '@expo/vector-icons/Entypo';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { Link } from 'expo-router';
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Home() {
+
+
+
   return (
     <ScrollView style={styles.mainContainer}>
       {/* Header */}
@@ -20,11 +25,11 @@ export default function Home() {
         {/* Profile Pic + Name */}
         <View style={styles.userProfileContainer}>
           {/* Profile Pic */}
-          <View>
-            <AntDesign name="user" size={24} color="black"/>
+          <View style={styles.userProfileImage}>
+            <Ionicons name="person" size={24} color="#543cda" />
           </View>
           {/* Greeting + Name */}
-          <Text style={styles.userProfileText}> Hi, User! </Text>
+          <Text style={styles.userProfileText}> Hi, Alex! </Text>
         </View>
 
         {/* Header Text/Title */}
@@ -41,20 +46,89 @@ export default function Home() {
             <FontAwesome name="trophy" size={40} color="#ffd900ff" />
             <Text style={statsStyles.statsMainText}> 7 Days </Text>
             <Text style={statsStyles.statsSubText}> Streak </Text>
-
           </View>
 
           {/* Average Score */}
           <View style={statsStyles.statsBox}>
             <Entypo name="area-graph" size={40} color="#ffd900ff" />
-            <Text style={statsStyles.statsMainText}> 10.3% </Text>
+            <Text style={statsStyles.statsMainText}> 90% </Text>
 
           </View>
+        </View>
+          <Link href={'/progress'}> 
+            <Text style={{...styles.subHeaderText, textDecorationLine:'underline'}}>Wanna check your progress?</Text>
+          </Link>
+      </View>
 
+      {/* Main Body */}
+      <View style={styles.bodyContainer}>
+        {/* Begin Random Quiz button */}
+        <TouchableOpacity style={{backgroundColor: "#715bf0ff", padding: 5, borderRadius: 20, borderWidth: 2, borderColor: "#543cda"}}>
+          <Text style={{textAlign: 'center', fontSize: 20, color: "#ffff"}}>Begin Random Quiz</Text>
+        </TouchableOpacity>
+
+        {/* Calculus Basics */}
+        <Text style={styles.bodyHeader}> Continue Learning </Text>
+        <TouchableOpacity style={styles.bodyElement}>
+          <View style={{flexDirection: 'row'}}>
+            {/* Topic Icon */}
+            <MaterialCommunityIcons name="math-integral-box" size={40} color="#ff0000ff" style={{alignSelf: 'center'}}/>
+            <View style={{alignItems:'flex-start'}}>
+              {/* Topic Header */}
+              <Text style={styles.bodyElementText}> Calculus Basics </Text>
+              {/* Questions Done */}
+              <Text style={{paddingLeft: 10}}>19/20 Questions</Text>
+              {/* Progress Bar */}
+              <View style={{...styles.progressBar, marginLeft: 10}}>
+                <View style={{...styles.progressFill, backgroundColor: '#ff0000ff', width: '95%'}}></View>
+              </View>
+            </View>
+          </View>
+          <Entypo name="chevron-right" size={15} color="black" style={{alignSelf: 'center'}}/>
+        </TouchableOpacity>
+
+        {/* History */}
+        <TouchableOpacity style={styles.bodyElement}>
+          <View style={{flexDirection: 'row'}}>
+            {/* Topic Icon */}
+            <Entypo name="globe" size={40} color="#543cda" style={{alignSelf: 'center'}}/>
+            <View style={{alignItems:'flex-start'}}>
+              {/* Topic Header */}
+              <Text style={styles.bodyElementText}> History </Text>
+              {/* Questions Done */}
+              <Text style={{paddingLeft: 10}}>18/20 Questions</Text>
+              {/* Progress Bar */}
+              <View style={{...styles.progressBar, marginLeft: 10}}>
+                <View style={{...styles.progressFill, backgroundColor: '#543cda', width: '90%'}}></View>
+              </View>
+            </View>
+          </View>
+          <Entypo name="chevron-right" size={15} color="black" style={{alignSelf: 'center'}}/>
+        </TouchableOpacity>
+
+        {/* Categories Section */}
+        <Text style={styles.bodyHeader}> Explore Categories </Text>
+        <View style={categories.categoriesRow}>
+          <CategoryCard topic="Science" numberQuizzes='20+'/>
+          <CategoryCard topic="History" numberQuizzes='20+'/>
+        </View>
+        <View style={categories.categoriesRow}>
+          <CategoryCard topic="Elden Theory" numberQuizzes='15'/>
+          <CategoryCard topic="Ring Theory" numberQuizzes='10'/>
+        </View>
+        <View style={categories.categoriesRow}>
+          <CategoryCard topic="Biology" numberQuizzes='20+'/>
+          <CategoryCard topic="Trivia" numberQuizzes='20+'/>
+        </View>
+        <View style={categories.categoriesRow}>
+          <CategoryCard topic="Calculus" numberQuizzes='20+'/>
+          <CategoryCard topic="Computer Science" numberQuizzes='20'/>
         </View>
 
       </View>
-      <Link href={'/progress'}> <Text>to progress</Text></Link>
+      
+      {/* Padding for the bottom */}
+        <View style={{paddingBottom: 100}}></View>
     </ScrollView>
   );
 }
@@ -84,15 +158,62 @@ const styles = StyleSheet.create({
   },
   userProfileContainer: {
     width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'flex-start'
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5
+
+  },
+  userProfileImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 999,
+    backgroundColor: "#d8e0ff",
+    justifyContent: "center",
+    alignItems: "center",
 
   },
   userProfileText: {
     color: "#ffff",
-    fontSize: 18
+    fontSize: 16
 
-  }
+  },
+  bodyContainer: {
+    margin: 10,
+    flexGrow: 1,
+    gap: 5
+
+  },
+  bodyHeader: {
+    fontSize: 16,
+
+  },
+  bodyElement: {
+    backgroundColor: "#ffffffff",
+    padding: 10,
+    borderRadius: 20,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: "#e5e7eb",
+  },
+  bodyElementText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    padding: 5,
+    paddingBottom: 0
+  },
+  progressBar: {
+    backgroundColor: "#e6e6e6",
+    width: 220,
+    height: 10,
+    borderRadius: 20,
+    overflow: "hidden",
+    zIndex: 0,
+  },
+  progressFill: {
+    height: "100%",
+  },
   
 
 })
@@ -125,4 +246,11 @@ const statsStyles = StyleSheet.create({
     color: "#ffffffff",
     fontWeight: 'thin'
   }
+})
+
+const categories = StyleSheet.create({
+  categoriesRow: {
+    flexDirection: 'row', 
+    justifyContent:'space-between'
+  },
 })
---
+++ components/CategoryCard.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+interface Props {
+    topic: string
+    numberQuizzes: string
+}
+
+const CategoryCard = ({topic, numberQuizzes}: Props) => {
+  return (
+    <TouchableOpacity style={categories.categoriesCard}>
+        <View style={{alignContent: 'center'}}>
+            <Text style={categories.categoriesHeader}>{topic}</Text>
+            <Text style={categories.categoriesSubHeader}>{numberQuizzes} quizzes</Text>
+        </View>
+    </TouchableOpacity>
+  )
+}
+
+export default CategoryCard
+
+const categories = StyleSheet.create({
+  categoriesContainer: {
+    flexDirection: 'row', 
+    justifyContent:'space-between'
+  },
+  categoriesCard: {
+    backgroundColor: "#ffffffff",
+    padding: 15,
+    borderRadius: 20,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: "#e5e7eb",
+    flex: 1,
+    justifyContent: 'center',
+  },
+  categoriesHeader: {
+    textAlign:'center', 
+    fontSize: 18,
+    fontWeight: 'semibold'
+  }, 
+  categoriesSubHeader: {
+    textAlign:'center', 
+    fontSize: 12
+  }
+})
